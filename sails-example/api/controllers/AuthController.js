38b7eecb6758632e190a38c3f6f532e41837d433
yotam shalev <yotamsha@gmail.com>
2015-11-13 21:20:33 +0200

    Adding missing files for:
    'Created example API for Users and Tasks in the sailsJs example.
    Added example of how to work with passportjs. '

---
+++ sails-example/api/controllers/AuthController.js
@@ -0,0 +1,80 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var passport = require('passport');
+
+module.exports = {
+
+  _config: {
+    actions: false,
+    shortcuts: false,
+    rest: false
+  },
+
+  login: function(req, res) {
+
+    passport.authenticate('local', function(err, user, info) {
+      sails.log.info("trying to login..");
+      if ((err) || (!user)) {
+        sails.log.error("login failed..", err, user);
+
+        return res.send({
+          message: info.message,
+          user: user
+        });
+      }
+      req.logIn(user, function(err) {
+        if (err) res.send(err);
+        return res.send({
+          message: info.message,
+          user: user
+        });
+      });
+
+    })(req, res);
+  },
+
+  logout: function(req, res) {
+    req.logout();
+    res.redirect('/');
+  }
+};
+
+/**
+ * Sails controllers expose some logic automatically via blueprints.
+ *
+ * Blueprints are enabled for all controllers by default, and they can be turned on or off
+ * app-wide in `config/controllers.js`. The settings below are overrides provided specifically
+ * for AuthController.
+ *
+ * NOTE:
+ * REST and CRUD shortcut blueprints are only enabled if a matching model file
+ * (`models/Auth.js`) exists.
+ *
+ * NOTE:
+ * You may also override the logic and leave the routes intact by creating your own
+ * custom middleware for AuthController's `find`, `create`, `update`, and/or
+ * `destroy` actions.
+ */
+
+module.exports.blueprints = {
+
+  // Expose a route for every method,
+  // e.g.
+  // `/auth/foo` => `foo: function (req, res) {}`
+  actions: true,
+
+  // Expose a RESTful API, e.g.
+  // `post /auth` => `create: function (req, res) {}`
+  rest: true,
+
+  // Expose simple CRUD shortcuts, e.g.
+  // `/auth/create` => `create: function (req, res) {}`
+  // (useful for prototyping)
+  shortcuts: true
+
+};
