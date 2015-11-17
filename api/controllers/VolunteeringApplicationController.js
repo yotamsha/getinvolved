9abe415778283b930d22866ff81ef2eac704ace7
yotam shalev <yotamsha@gmail.com>
2015-11-18 00:10:07 +0200

    [adding missing files for commit] Added swagger screen to git. added a new api endpoint for volunteers application form.

---
+++ api/controllers/VolunteeringApplicationController.js
@@ -0,0 +1,11 @@
+/**
+ * VolunteeringApplicationController
+ *
+ * @description :: Server-side logic for managing volunteeringapplication
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
