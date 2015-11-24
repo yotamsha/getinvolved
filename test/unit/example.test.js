d752a0ecd5a7737c88e06fa3d227454692131234
yotam shalev <yotamsha@gmail.com>
2015-11-25 01:07:32 +0200

    Added tests infrastructure for creating unit tests using mocha.

---
+++ test/unit/example.test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yotam on 25/11/2015.
+ */
+describe('UsersModel', function() {
+
+  describe('#find()', function() {
+
+    it('should check find function', function () {
+     console.log("pass");
+    });
+  });
+
+});
