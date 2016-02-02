b900e7fb6ce12535153f6f25319d9fbbed408f14
yonidor <yonidor@gmail.com>
2016-02-02 23:38:03 +0200

    add grunt-http-server to grunt

--- clients/web/Gruntfile.js
+++ clients/web/Gruntfile.js
@@ -13,10 +13,31 @@ module.exports = function(grunt) {
               {src: 'app/assets/css/app.scss', dest: 'app/assets/css/app.css'},
             ],
          }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+          livereload: true,
+        },
+      },
+    },
+    'http-server': {
+       'dev': {
+           root: "app",
+           port: 8000,
+           host: "127.0.0.1",
+           showDir : true,
+           autoIndex: true,
+           ext: "html",
+           openBrowser : true
+       }
     }
   });
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
-
+  grunt.registerTask('start', ['http-server:dev']);
 };
