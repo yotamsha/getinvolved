1a5df003556a65a70afed9de7eb3e8e4736972f7
yonidor <yonidor@gmail.com>
2016-02-09 23:46:33 +0200

    configure restangular and make first connection with server!

--- clients/web/app/casesSearch/casesSearchCtrl.js
+++ clients/web/app/casesSearch/casesSearchCtrl.js
@@ -12,29 +12,41 @@ angular.module('app.casesSearch', [])
             controller: 'casesSearchCtrl'
         });
     }])
-
+    .config(function(RestangularProvider) {
+        RestangularProvider.setBaseUrl('http://localhost:5000/api');
+        RestangularProvider.setDefaultHeaders({
+           "Authorization": "Basic YWRtaW46YWRtaW4="
+         });
+    })
     .controller('casesSearchCtrl', ['$scope', 'Restangular', '$stateParams','DialogsService','moment',
         function ($scope, Restangular, $stateParams, DialogsService, moment) {
 
-            // --- INNER FUNCTIONS --- //
-            function createCasesArr(){
-              var arr = [];
-              for (var i = 1; i < 24; i++) {
-                  arr.push({
-                    title: ' עזרה בהסעה ' + i,
-                    imgSrc: 'assets/img/face1.jpg',
-                    order: i,
-                  });
-              }
 
-              return arr;
-            }
+
+            // --- INNER FUNCTIONS --- //
+            // function createCasesArr(){
+            //   var arr = [];
+            //   for (var i = 1; i < 24; i++) {
+            //       arr.push({
+            //         title: ' עזרה בהסעה ' + i,
+            //         imgSrc: 'assets/img/face1.jpg',
+            //         order: i,
+            //       });
+            //   }
+            //
+            //   return arr;
+            // }
 
             function _init() {
                 $scope.vm = {
-                  cases : createCasesArr(),
+                  cases : [],
                   reverse: false
                 };
+
+                var baseCases = Restangular.all('cases');
+                baseCases.getList().then(function(cases){
+                  $scope.vm.cases = cases;
+                });
 
                 $scope.vm.changeSort = function(isReversed){
                     $scope.vm.reverse = isReversed;
