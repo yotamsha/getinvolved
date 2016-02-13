f527026c7f123f5f28e13ea66ff48ed1cdac669f
yonidor <yonidor@gmail.com>
2016-02-13 16:39:47 +0200

    add basic paging

--- clients/web/app/app.js
+++ clients/web/app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('app', [
         // External modules
+        'angularUtils.directives.dirPagination',
         'ngRoute',
         'ngMaterial',
         'ngCookies',
