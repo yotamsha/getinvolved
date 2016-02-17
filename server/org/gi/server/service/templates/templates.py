8f755adf127dcdf7f5c29927c77d25beb4e4b04e
Avishay Balderman <balderman@gmail.com>
2016-02-17 23:00:51 +0200

    Serrver notifications - load tamplates and merge #189

---
+++ server/org/gi/server/service/templates/templates.py
@@ -0,0 +1,55 @@
+__author__ = 'avishayb'
+
+from jinja2 import Template, Environment, StrictUndefined, FileSystemLoader
+import os
+
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class GIFileSystemLoader(FileSystemLoader):
+    def __init__(self, root_folder, lang='en'):
+        super(GIFileSystemLoader, self).__init__(root_folder + '/data/' + lang)
+        print('---> ' + self.searchpath[0])
+
+    def get_source(self, environment, template):
+        return super(GIFileSystemLoader, self).get_source(_j2_env, template)
+
+
+_j2_env = Environment(loader=None, undefined=StrictUndefined,
+                      trim_blocks=True, cache_size=0)
+
+
+def merge(template_body, data):
+    """
+    Merge a template
+    :param template_body:  the template body
+    :param data: data to be used for merge
+    :return: the merged template
+    """
+    if not template_body or not isinstance(template_body, str):
+        raise Exception('template body must be none empty string')
+    if not data or not isinstance(data, dict):
+        raise Exception('data must be none empty dict')
+    template = Template(template_body, undefined=StrictUndefined)
+    return template.render(data)
+
+
+def load_and_merge(template_name, data, lang='en'):
+    """
+    Load and Merge a template
+    :param template_name:  the template name
+    :param data: data to be used for merge
+    :param lang: the template language (this used as a sub folder under data folder)
+    :return: the merged template
+    """
+    if not template_name or not isinstance(template_name, str):
+        raise Exception('template_name  must be none empty string')
+    if not data or not isinstance(data, dict):
+        raise Exception('data must be none empty dict')
+    _j2_env.loader = GIFileSystemLoader(THIS_DIR, lang=lang)
+    template = _j2_env.get_template(template_name)
+    return template.render(data)
+
+
+
--- server/org/gi/tests/template_tests.py
+++ server/org/gi/tests/template_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from org.gi.server.services.templates import merge
+from org.gi.server.service.templates.templates import merge, load_and_merge
 
 
 class GITemplateTestCase(unittest.TestCase):
@@ -19,5 +19,17 @@ class GITemplateTestCase(unittest.TestCase):
 
     def test_merge_fail_4(self):
         self.assertRaises(Exception, merge, 'Hello {{name}}', {'___name': 'jack'})
+
+    def test_load_and_merge(self):
+        self.assertEqual(load_and_merge('test_only.txt', {'name': 'jack'}), 'Hello jack')
+
+    def test_load_and_merge_fail_1(self):
+        self.assertRaises(Exception, load_and_merge, 'test_only.txt', {'___name': 'jack'})
+
+    def test_load_and_merge_fail_2(self):
+        self.assertRaises(Exception, load_and_merge, 'test_only.txt', ['jack'])
+
+    def test_load_and_merge_fail_3(self):
+        self.assertRaises(Exception, load_and_merge, 'test_only.txt', {'name': 'jack'}, lang='xyz')
 
 
