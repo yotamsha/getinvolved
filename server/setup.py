55615720952a3b61928207943406148484d42571
Avishay Balderman <balderman@gmail.com>
2016-03-15 21:57:29 +0200

    server package WIP #240

--- server/setup.py
+++ server/setup.py
@@ -1,10 +1,27 @@
+import os
 from setuptools import setup
 
+
+for dirName, subdirList, fileList in os.walk('org/gi/server/service/templates/data'):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+        print('\t%s' % fname)
+
+
+
 setup(
-    name='GI Server',
+    name='get_involved_server',
     version='1.0',
     long_description=__doc__,
-    packages=['org.gi.config','org.gi.server'],
+    packages=['org.gi.config',
+              'org.gi.server',
+              'org.gi.server.api',
+              'org.gi.server.model',
+              'org.gi.server.service.notification',
+              'org.gi.server.service.templates',
+              'org.gi.server.validation',
+              'org.gi.server.validation.task',
+              'org.gi.server.model'],
     include_package_data=True,
     zip_safe=False,
     install_requires=['flask',
