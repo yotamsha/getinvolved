0b70d4b99b252433676775e8320585c9c67012db
Yagiro <yagiro@gmail.com>
2016-03-20 20:56:06 +0200

    Some fixes to Help Request Form

--- clients/web/app/views/ask-help/askHelp.js
+++ clients/web/app/views/ask-help/askHelp.js
@@ -15,7 +15,14 @@ angular.module('app.views.helpRequestForm', [])
             var vm = this;
 
             vm.imgurl = "http://s9.postimg.org/47m1pb81b/vol.png";
-
             vm.translatePath = 'views.askHelp';
+            vm.periods = [
+                'views.askHelp.hr1',
+                'views.askHelp.hr2',
+                'views.askHelp.hr3',
+                'views.askHelp.hr4-8',
+                'views.askHelp.dy0.5',
+                'views.askHelp.dy1',
+            ]
         }
     ]);
