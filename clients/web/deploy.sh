f932ee5c9ba2cb298141733116703f2a175d8afb
Avishay Balderman <balderman@gmail.com>
2016-03-22 22:42:55 +0200

    web client deploy script

--- clients/web/deploy.sh
+++ clients/web/deploy.sh
@@ -9,19 +9,24 @@ set -x
 ZONE=europe-west1-b
 CLIENT_ARCHIVE=get_involved_client.tar.gz
 INSTANCE_NAME=gi-server
-REMOTE_SCRIPT=remote_deploy.sh
+REMOTE_SCRIPT=client_remote_deploy.sh
 REMOTE_PATH=/var/wwww/html
 
 python zipit.py ${CLIENT_ARCHIVE} ./app
-
-gcloud compute copy-files ./${CLIENT_ARCHIVE} ${INSTANCE_NAME}:${REMOTE_APTH} --zone ${ZONE}
-#gcloud compute copy-files ./${REMOTE_SCRIPT} ${INSTANCE_NAME}:${REMOTE_PATH} --zone ${ZONE}
-
+echo $?
+gcloud compute copy-files ./${CLIENT_ARCHIVE} ${INSTANCE_NAME}:~/ --zone ${ZONE}
+echo $?
+gcloud compute copy-files ./${REMOTE_SCRIPT} ${INSTANCE_NAME}:~/ --zone ${ZONE}
+echo $?
 rm -rf ${CLIENT_ARCHIVE}
-
+echo $?
 # run remote script
-gcloud compute ssh ${INSTANCE_NAME} --zone ${ZONE}  sudo mv ~/${CLIENT_ARCHIVE} ${REMOTE_APTH}
-#gcloud compute ssh ${INSTANCE_NAME} --zone ${ZONE}  ${REMOTE_SCRIPT}/${REMOTE_SCRIPT}
+gcloud compute ssh ${INSTANCE_NAME} --zone ${ZONE} chmod +x ./${REMOTE_SCRIPT}
+echo $?
+gcloud compute ssh ${INSTANCE_NAME} --zone ${ZONE}  ./${REMOTE_SCRIPT}
+echo $?
+
+
 
 
 
