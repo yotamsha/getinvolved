132b851339d3a95d68181596f3526132fe11aecc
Avishay Balderman <balderman@gmail.com>
2016-04-01 13:00:44 +0300

    More UT for len validations

--- server/org/gi/tests/validation_tests.py
+++ server/org/gi/tests/validation_tests.py
@@ -12,14 +12,23 @@ class ValidationsTest(unittest.TestCase):
         self.assertFalse(validations.is_user_id('54f0e5aa313f5d824680d'))
 
     def test_len_range_unicode_support(self):
-        description = u'אם חד הורית עם מוגבלות פיזית זקוקה לעזרה בפריקת ארגזים לאחר מעבר דירה'
-        self.assertTrue(validation_utils.validate_len_in_range('task', 'description', description))
+        description_heb = u'אם חד הורית עם מוגבלות פיזית זקוקה לעזרה בפריקת ארגזים לאחר מעבר דירה'
+        self.assertTrue(validation_utils.validate_len_in_range('task', 'description', description_heb))
+        description_spa = u'Érase una vez que había una mamá cerda que tenía tres cerditos'
+        self.assertTrue(validation_utils.validate_len_in_range('task', 'description', description_spa))
+
 
     def test_entity_not_found(self):
         self.assertRaises(ValueError, validation_utils.validate_len_in_range, '__u_cant_find_me', '__', '')
 
     def test_field_not_found(self):
         self.assertRaises(ValueError, validation_utils.validate_len_in_range, 'user', 'no_way..', '')
+
+    def test_value_is_none(self):
+        self.assertRaises(ValueError, validation_utils.validate_len_in_range, 'user', 'description', None)
+
+    def test_value_is_dict(self):
+        self.assertRaises(ValueError, validation_utils.validate_len_in_range, 'user', 'description', {})
 
     def test_len_range_short(self):
         self.assertFalse(validation_utils.validate_len_in_range('task', 'description', 'short'))
