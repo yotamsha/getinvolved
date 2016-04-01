03a2db7a6714960890793723420a474fcb3f670d
BarWachtel <barwachtel@gmail.com>
2016-04-01 03:47:01 +0300

    Insert demo. data into DB fixed

--- server/db_dev_data.py
+++ server/db_dev_data.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python
-import getopt
-
 import requests
 import json
 import time
-import argparse
-
-import sys
 
 from org.gi import args
 from org.gi.server.db import mongo
 from org.gi.server.utils import get_hours_in_seconds
 from org.gi.server.validation.task.task_state_machine import TASK_ASSIGNED
-from org.gi.tests.misc import ACCESS_TOKEN_AUTH, SERVER_URL_API
+from org.gi.server.web_token import AccessTokenAuth
 
-access_token = ACCESS_TOKEN_AUTH
+SERVER_URL = 'http://localhost:5000'
+SERVER_URL_API = 'http://localhost:5000/api'
 
 
 def next_due_date():
@@ -26,10 +22,21 @@ next_due_date.counter = 0
 
 
 def load_db():
+    global access_token
+    users_endpoint = '%s/users' % SERVER_URL_API
+    with open("dev_data/first_user.json") as _user:
+        user = json.load(_user)
+        r = requests.post(users_endpoint, json=user)
+        user_credentials = {
+            'username': user['user_name'],
+            'password': user['password']
+        }
+        r = requests.get('%s/login' % SERVER_URL, params=user_credentials)
+        access_token = AccessTokenAuth(r.content)
+
     petitioner_ids = []
     with open("dev_data/petitioners.json", "r") as _users:
         users = json.load(_users)
-        users_endpoint = '%s/users' % SERVER_URL_API
         insert_entities(users_endpoint, petitioner_ids, users)
 
     case_ids = []
@@ -49,17 +56,18 @@ def load_db():
         insert_entities(users_endpoint, volunteer_ids, users)
 
     for index in range(len(case_ids) - 1):
-        r = requests.get(cases_endpoint + "/{}".format(case_ids[index]), auth=ACCESS_TOKEN_AUTH)
+        r = requests.get(cases_endpoint + "/{}".format(case_ids[index]), auth=access_token)
         case = json.loads(r.content)
         for volunteer_id, task in zip(volunteer_ids, case['tasks']):
             task['volunteer_id'] = volunteer_id
             task['state'] = TASK_ASSIGNED
-        requests.put(cases_endpoint + "/{}".format(case_ids[index]), auth=ACCESS_TOKEN_AUTH, json=case)
+        requests.put(cases_endpoint + "/{}".format(case_ids[index]), auth=access_token, json=case)
 
 
 def insert_entities(endpoint, entity_ids, entities):
     for entity in entities:
-        r = requests.post(endpoint, auth=ACCESS_TOKEN_AUTH, json=entity)
+        r = requests.post(endpoint, auth=access_token, json=entity)
+        print "{} {}".format(r.content, r.status_code)
         if r.status_code == 201:
             json_user = json.loads(r.content)
             entity_ids.append(json_user['id'])
